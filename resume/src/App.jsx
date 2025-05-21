6187aac3f1101f208bf1ea091ca3a6cb9da25621
soyeon1806 <ksy1118s@naver.com>
2025-05-21 14:35:39 +0900

    ✨ feat: 라우팅 설정 추가 및 메인 페이지 연결

--- resume/src/App.jsx
+++ resume/src/App.jsx
@@ -1,8 +1,18 @@
-import LoginPage from './pages/login/LoginPage';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import LoginPage from '@/pages/login/LoginPage'; // 네가 만들었던 로그인
+import MainPage from '@/pages/main/MainPage';
 import React from 'react';
 
-function App(){
-  return <LoginPage />;
-}
+const App = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<MainPage />} />
+                <Route path="/login" element={<LoginPage />} />
+            </Routes>
+        </BrowserRouter>
+    );
+};
 
 export default App;
